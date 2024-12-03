66b5c5354273e9568d6b146fb7b6b17812a5c9cb
Ocean <mintteacupofficial@gmail.com>
2024-12-03 18:25:56 +0000

    added card component

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -3,9 +3,9 @@ import profilePic from "./assets/brendanfraserlokivariant.png"
 function Card(){
     return(
         <div className="card">
-            <img src={profilePic} alt="profile picture"></img>
-            <h2>Mintteacup</h2>
-            <p>I am a student studying Video Games Development and i am learning web development</p>
+            <img className="card-image" src={profilePic} alt="profile picture"></img>
+            <h2 className="card-title">@Mintteacup</h2>
+            <p className="card-text">I am a Video Games Development student and I am learning web development</p>
         </div>
     );
 }
