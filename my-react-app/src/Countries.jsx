13e9470b173b418e9aa46cbff9b3f756c7511942
Ocean <mintteacupofficial@gmail.com>
2024-12-03 00:17:20 +0000

    added countries component

--- my-react-app/src/Countries.jsx
+++ my-react-app/src/Countries.jsx
@@ -1,16 +1,19 @@
 
 function Countries(){
     
-    const country1 = "England"; 
+    const country1 = "Hawaii"; 
     const country2 = "Australia";
     const country3 = "South Korea";
 
     return(
+        <>
+        <h2>Countries I Want To Visit</h2>
         <ul>
-            <li>{country1}</li>
-            <li>{country2.toLowerCase()}</li>
+            <li>{country1.toUpperCase()}</li>
+            <li>{country2.toUpperCase()}</li>
             <li>{country3.toUpperCase()}</li>
         </ul>
+        </>
     );
 }
 
